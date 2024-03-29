db226d3ca88a463f57384b0c1ff21be2ae6807a4
doniduaasattuu <doni.duaasattuu@gmail.com>
2024-03-29 15:13:39 +0700

    create dart library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello('Doni'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
+
+  print(customer);
+  print(category);
+  print(product);
+}
